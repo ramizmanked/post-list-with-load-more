23100f292562da5a18eec52072e1d1739292ff05
Ramiz Manked <ramiz.manked@gmail.com>
2022-12-17 20:55:26 +0530

    Update plugin bootstrap doc comment

--- post-list-with-load-more.php
+++ post-list-with-load-more.php
@@ -6,8 +6,8 @@
  * Version:           1.0.5
  * Author:            Ramiz Manked
  * Author URI:        https://ramizmanked.com
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           GPLv3
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain:       post-list-with-load-more
  * Domain Path:       /languages
  *
